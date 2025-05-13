249694bc6869b9f191ed8bb02b76ac3c083bfc4a
Jean Colonia <engshka123@gmail.com>
2025-05-12 22:19:13 -0500

    bronze sql files

---
+++ demo/models/bronze/customers/br_customers.sql
@@ -0,0 +1,5 @@
+
+/* Creating view in DB (w.o modifying raw data) **/
+-- Usando ref porque la fuente es un archivo que gestiona y controla dbt, caso contrario source().
+
+SELECT * FROM {{ref( 'customers')}}
