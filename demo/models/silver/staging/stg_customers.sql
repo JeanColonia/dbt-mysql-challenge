c1afaf3bd8d43da7cd28967cee429f2fadc6be95
Jean Colonia <engshka123@gmail.com>
2025-05-17 17:02:20 -0500

    generating stg_products script

--- demo/models/silver/staging/stg_customers.sql
+++ demo/models/silver/staging/stg_customers.sql
@@ -1,10 +1,22 @@
 
-with raw_customers as(
-    select * from {{ref('raw_customers')}}
+WITH raw_customers AS(
+    SELECT * FROM {{ref('raw_customers')}}
 )
 
 
-SELECT customer_id, TRIM(name) as customer_name, TRIM(email) as customer_email, TRIM(address) as customer_address, CAST(registration_date as DATE) as registration_date, TRIM(UCASE(segment)) as customer_segment from raw_customers where customer_id  not in (SELECT MAX(customer_id)
-FROM raw_customers WHERE customer_id IS NOT NULL
-GROUP BY name, email, address 
-HAVING COUNT(*) > 1)
+SELECT 
+    customer_id, 
+    TRIM(name) as customer_name, 
+    TRIM(email) as customer_email, 
+    TRIM(address) as customer_address, 
+    CAST(registration_date as DATE) as registration_date, 
+    TRIM(UCASE(segment)) as customer_segment 
+FROM raw_customers 
+WHERE customer_id  
+NOT IN (
+        SELECT MAX(customer_id)
+        FROM raw_customers 
+        WHERE customer_id IS NOT NULL
+        GROUP BY name, email, address 
+        HAVING COUNT(*) > 1
+       )
---
+++ demo/models/silver/staging/stg_products.sql
@@ -0,0 +1,24 @@
+
+
+WITH raw_products AS (
+    SELECT * FROM {{ref('raw_products')}}
+    
+)
+
+SELECT 
+    UCASE(TRIM(product_id)) as product_id, 
+    TRIM(name) as product_name, 
+    TRIM(description) as product_description, 
+    TRIM(category) as product_category, 
+    ROUND(price, 2) as product_price, 
+    UCASE(TRIM(sku)) as product_sku 
+FROM raw_products 
+WHERE product_id 
+NOT IN 
+    (   SELECT 
+            MAX(product_id) 
+        FROM raw_products 
+        WHERE product_id IS NOT NULL 
+        GROUP BY name, category, price 
+        HAVING COUNT(*) >1
+    )
