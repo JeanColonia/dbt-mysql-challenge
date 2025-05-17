1b23be876992db935982a75587e246a472008515
Jean Colonia <engshka123@gmail.com>
2025-05-17 17:45:19 -0500

    generating stg_orders and its tests

---
+++ demo/models/silver/staging/stg_orders.sql
@@ -0,0 +1,16 @@
+
+
+WITH 
+    raw_orders 
+AS 
+    (
+    SELECT * FROM {{ref('raw_orders')}}
+    )
+
+
+select 
+    UCASE(TRIM(order_id)) as order_id, 
+    customer_id, CAST(order_date as DATE) as order_date, 
+    UCASE(TRIM(order_status)) as order_status, 
+    TRIM(payment_method) as payment_method
+FROM raw_orders
