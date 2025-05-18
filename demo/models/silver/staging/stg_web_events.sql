2cd11d82bc885dad37544c3b0a345ddfcb4e29f7
Jean Colonia <engshka123@gmail.com>
2025-05-17 19:30:07 -0500

    fixing regex web url in stg_web_events

--- demo/models/silver/staging/stg_web_events.sql
+++ demo/models/silver/staging/stg_web_events.sql
@@ -11,5 +11,5 @@ SELECT
     UCASE(TRIM(session_id)) as web_session_id, 
     customer_id, CONVERT(timestamp, DATETIME) as web_session_date, 
     TRIM(event_type) as web_event_type, 
-    TRIM(url) as web_url 
+    CASE WHEN TRIM(url) NOT LIKE '/%' THEN CONCAT('/', url) ELSE url END as web_url
 FROM raw_web_events
