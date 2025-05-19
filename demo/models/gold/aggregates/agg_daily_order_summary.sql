01864c76f0f678ec8f46416cfa98ba71aec64c35
Jean Colonia <engshka123@gmail.com>
2025-05-19 01:24:53 -0500

    generating daily order summary

---
+++ demo/models/gold/aggregates/agg_daily_order_summary.sql
@@ -0,0 +1,36 @@
+
+
+WITH sales_by_day 
+AS
+(
+    SELECT 
+        CAST(order_date AS DATE) AS order_day,
+        COUNT(*) AS total_number_of_orders,
+        ROUND(SUM(total_amout), 2) AS total_revenue,
+        ROUND(AVG(total_amout), 2) AS avg_orders_value,
+        COUNT(DISTINCT customer_id) AS total_unique_customers,
+        COUNT(customer_id) AS total_customers
+    FROM 
+        {{(ref('fct_orders'))}} 
+    GROUP BY 
+        order_day 
+    ORDER BY 
+        order_day 
+    ASC
+)
+
+
+    SELECT 
+        order_day,
+        total_number_of_orders,
+        total_revenue,
+        avg_orders_value,
+        total_unique_customers,
+        total_customers
+    FROM 
+         sales_by_day
+    GROUP BY 
+        order_day 
+    ORDER BY 
+        order_day 
+    ASC
